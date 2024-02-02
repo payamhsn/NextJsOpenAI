c9278ef668e7ca979cf9be8cbd0bd50a9d73ff9b
Payam <p.hoseini.ch@gmail.com>
2024-02-02 15:55:57 +0330

    Create home page, put 3 logo and make their links.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,27 @@
-export default function home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import Link from "next/link";
+import stacks from "../data/stacks.json";
+import Image from "next/image";
+
+export default function Home() {
+  const renderStacks = () => {
+    return Object.keys(stacks).map((stackKey) => {
+      const stack = stacks[stackKey];
+      return (
+        <Link
+          key={stack.href}
+          href={stack.href}
+          className="w-20 h-20 relative border-2 border-solid m-2 rounded-xl"
+        >
+          <Image src={stack.logo} className="object-cover p-2" fill alt="" />
+        </Link>
+      );
+    });
+  };
+
+  return (
+    <div className="h-full flex justify-center items-center flex-col">
+      <div>What do you want to learn?</div>
+      <div className="flex">{renderStacks()}</div>
+    </div>
+  );
 }
