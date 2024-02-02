b156e54bcdc2238578169f37dc558b1d49a69abc
Payam <p.hoseini.ch@gmail.com>
2024-02-02 17:03:56 +0330

    Create dynamic path for stacks pages and create a header for them/

---
+++ src/pages/learning/[stack].js
@@ -0,0 +1,33 @@
+import stacks from "../../data/stacks.json";
+import Image from "next/image";
+
+export default function Stack({ stack, stackKey }) {
+  return (
+    <div className="h-full flex flex-col">
+      <div className="header flex bg-slate-200 p-4 rounded-2xl">
+        <div className="flex mr-4 justify-center items-center">
+          <Image src={stack.logo} width={200} height={200} alt="" />
+        </div>
+        <div className="flex font-bold text-sm">{stack.info}</div>
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = Object.keys(stacks).map((key) => ({ params: { stack: key } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      stack: stacks[params.stack],
+      stackKey: params.stack,
+    },
+  };
+}
