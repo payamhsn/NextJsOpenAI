86d542bc2393136949fa918f56c34a438b7846cc
Payam <p.hoseini.ch@gmail.com>
2024-02-02 19:12:21 +0330

    Create API and route to serve res and req.

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,29 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function Message({ text, avatar, idx }) {
+export default function Message({ text: initialText, avatar, idx, author }) {
+  const [text, setText] = useState(author === "ai" ? "" : initialText);
   const bgColorClass = idx % 2 === 0 ? "bg-slate-100" : "bg-slate-200";
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setText(initialText.slice(0, text.length + 1));
+    }, 25);
+
+    return () => clearTimeout(timeout);
+  });
+
+  const blinkingCursorClass =
+    initialText.length === text.length ? "" : "blinking-cursor";
 
   return (
     <div className={`flex flex-row ${bgColorClass} p-4`}>
       <div className="w-[30px] relative mr-4">
         <Image src={avatar} width={30} height={30} alt="" />
       </div>
-      <div className="w-full">{text}</div>
+      <div className="w-full">
+        <div className={blinkingCursorClass}>{text}</div>
+      </div>
     </div>
   );
 }
---
+++ src/pages/api/completion.js
@@ -0,0 +1,14 @@
+export default async function completion(req, res) {
+  if (req.method === "POST") {
+    const body = req.body;
+    const prompt = body.prompt || "";
+
+    const aiResponse = "React JS is a library for creating UIs...";
+
+    await new Promise((res) => setTimeout(res, 500));
+
+    return res.status(200).json({ result: aiResponse });
+  } else {
+    return res.status(500).json({ error: { message: "Invalid Api Route" } });
+  }
+}
